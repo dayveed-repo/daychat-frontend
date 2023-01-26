c6e9d91add70fddf54eca29188a4665ce28c5237
Ebireni David <ebirenidavid@gmail.com>
2023-01-26 18:40:55 +0100

    26/1/2023 deploy commit

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://daychat.herokuapp.com",
+  baseURL: "https://daychat.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
